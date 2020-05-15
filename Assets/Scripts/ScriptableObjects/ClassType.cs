0029e2c5dca867d6fc8ad158a5735b0a3158b1aa
Parmpreet <parmpreet1337@gmail.com>
2020-05-15 14:23:42 -0700

    ClassType.cs now has a magicLevel subclass

--- Assets/Scripts/ScriptableObjects/ClassType.cs
+++ Assets/Scripts/ScriptableObjects/ClassType.cs
@@ -20,12 +20,20 @@ public class ClassType : ScriptableObject
          * 6        S
          */
     }
+
+    public class MagicLevel
+    {
+        public MagicType magicType;
+
+        public int maxLevel;
+    }
     
     public new string name;
     [TextArea]
     public string description;
     public TerrainType[] walkableTerrain; //The terrain this class can walk therough
     public WeaponLevel[] weaponLevels;
+    public MagicLevel[] magicLevels;
     public int hpGrowth, strGrowth, magGrowth, defGrowth, resGrowth, sklGrowth, spdGrowth;
     public int maxHp, maxStr, maxMag, maxDef, maxRes, maxSkl, maxSpd;
     public int mov;
