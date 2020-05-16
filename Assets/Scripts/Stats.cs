0a2e03e01b38fef32eb29b8c005e95cc99d754e5
parmpreet001 <parmppreet1337@gmail.com>
2020-05-15 17:25:08 -0700

    Formatting changes in Stats.cs

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    public bool isDead = false;
+    
     protected const int maxInventorySize = 5;
-   
     public int level = 1;
     public ClassType classType;
-    public SkillLevels skillLevels;
-    public bool isDead = false;
+    public SkillLevels skillLevels;    
+    public Weapon equippedWeapon;
+    
+    public Item[] inventory = new Item[maxInventorySize];
+    public List<Magic> magicList = new List<Magic>(); 
 
     public int baseHP, baseSTR, baseMAG, baseDEF, baseRES, baseSKL, baseSPD; //The base stats of the unit. ie their stats at level 1
     public int hp, str, mag, def, res, skl, spd; //The units current stats
     public int hpGrowth, strGrowth, magGrowth, defGrowth, resGrowth, sklGrowth, spdGrowth; //Growth rates of the unit.
-    public int maxHP;
-
-    //public int attackRangeMin; //mininum attack range of the unit
-    //public int attackRangeMax; //maximum attack range of the unit
-
-    //Equipped weapon
-    public Weapon equippedWeapon;
-
-    public Item[] inventory = new Item[maxInventorySize];
-    public List<Magic> magicList = new List<Magic>(); //List of spells this unit can potentionally learn
-
+    public int maxHP;  
+    
     public void Init()
     {
         hp = baseHP; str = baseSTR; mag = baseMAG; def = baseDEF; res = baseRES; skl = baseSKL; spd = baseSPD;
         maxHP = hp;
 
+        //Instanties each item in the unit's inventory. Items are scriptable objects
         for(int i = 0; i < 5; i++)
         {
             if(inventory[i] != null)
                 inventory[i] = Instantiate(inventory[i]);
         }
-        ((Weapon)inventory[0]).equipped = true;
-        equippedWeapon = ((Weapon)inventory[0]);
+
+        //Equip first weapon in inventory, if there is one
+        if(inventory[0] != null && inventory[0].GetType() == typeof(Weapon))
+        {
+            ((Weapon)inventory[0]).equipped = true;
+            equippedWeapon = ((Weapon)inventory[0]);
+        }
     }
 }
