e301ff4ee8799618b8a1cab8976f7891a7450d85
parmpreet001 <parmppreet1337@gmail.com>
2020-05-17 17:21:15 -0700

    Added durability variable to Magic.cs

--- Assets/Scripts/ScriptableObjects/Magic.cs
+++ Assets/Scripts/ScriptableObjects/Magic.cs
@@ -7,6 +7,7 @@ public abstract class Magic : ScriptableObject
     public new string name;
     public int minRequirement; //Mininum skill level needed to use this magic. Ranges from E to S, represented as an integer from 1 to 6
     public int[] numberOfUses = new int[6]; //Number of times this magic can be used, depending on the skill level of the user
+    public int durability; //Number of uses left per battle
     public TargetType targetType; //Whether the magic targets allies or enemies
     public MagicType magicType;
 
