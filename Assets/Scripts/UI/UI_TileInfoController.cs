373a56b69e716f5e4faf4462eb5284d8b6558e13
Parmpreet <parmpreet1337@gmail.com>
2020-07-10 22:18:46 -0700

    Added a Ui element to display tile info

---
+++ Assets/Scripts/UI/UI_TileInfoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_TileInfoController : MonoBehaviour
+{
+    private MapUIInfo mapUIInfo;
+    private UI_TileInfoDisplay tileInfoDisplay;
+    private Cursor cursor;
+
+    private void Start()
+    {
+        tileInfoDisplay = GetComponent<UI_TileInfoDisplay>();
+        mapUIInfo = GetComponentInParent<MapUIInfo>();
+        cursor = GameObject.Find("Cursor").GetComponent<Cursor>();
+    }
+
+    private void Update()
+    {
+        if(cursor.currentTile)
+        {
+            tileInfoDisplay.SetTileName(cursor.currentTile.terrainEffect.name);
+            tileInfoDisplay.SetTileInfo(cursor.currentTile.terrainEffect.description);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UI_TileInfoDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_TileInfoDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI tileName;
+    private TextMeshProUGUI tileInfo;
+
+    private void Start()
+    {
+        tileName = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        tileInfo = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetTileName(string tileName)
+    {
+        this.tileName.text = tileName;
+    }
+
+    public void SetTileInfo(string tileInfo)
+    {
+        this.tileInfo.text = tileInfo;
+    }
+}
