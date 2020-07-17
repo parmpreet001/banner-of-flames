5babb6e29272c501a109a8720979ba7186215a55
parmpreet001 <parmppreet1337@gmail.com>
2020-07-17 14:32:25 -0700

    HealForecast will only display if a unit is healing

--- Assets/Scripts/UI/UI_HealForecastController.cs
+++ Assets/Scripts/UI/UI_HealForecastController.cs
@@ -6,15 +6,27 @@ public class UI_HealForecastController : MonoBehaviour
 {
     private MapUIInfo mapUIInfo;
     private UI_HealForecastDisplay healForecastDisplay;
+    private Cursor cursor;
     void Start()
     {
         mapUIInfo = GetComponentInParent<MapUIInfo>();
         healForecastDisplay = transform.GetChild(0).GetComponent<UI_HealForecastDisplay>();
+        cursor = GameObject.Find("Cursor").GetComponent<Cursor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mapUIInfo.mapManager.CheckUnitStates(UnitStates.FINDING_ALLY, UnitStates.HEALING) && cursor.CurrentTileHasAllyUnit() 
+            && cursor.GetCurrentUnit() != mapUIInfo.selectedAllyUnit)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            healForecastDisplay.UpdateInfo(cursor.GetCurrentUnit().GetComponent<Stats>().hp,
+                cursor.GetCurrentUnit().GetComponent<Stats>().maxHP, 0);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
