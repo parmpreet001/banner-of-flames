54f2b8db5de7a450c4e9303cd488f459839e8222
Parmpreet <parmpreet1337@gmail.com>
2020-07-16 18:40:24 -0700

    Started making a Heal Forecast UI thing

---
+++ Assets/Scripts/UI/UI_HealForecastDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HealForecastDisplay : MonoBehaviour
+{
+    private Image CurrentHpHealthBar; //How much health the unit currently has
+    private Image HealedHpHealthBar; //How much health the unit will have after healing
+
+    private MapUIInfo mapUIInfo;
+    
+    private void Start()
+    {
+        mapUIInfo = GetComponentInParent<MapUIInfo>();
+        HealedHpHealthBar = transform.Find("HealedHP").GetComponent<Image>();
+        CurrentHpHealthBar = transform.Find("CurrentHP").GetComponent<Image>();
+
+        HealedHpHealthBar.fillAmount = 0;
+    }
+
+    private void Update()
+    {
+        if(mapUIInfo.selectedAllyUnit)
+        {
+            SetFillAmounts(mapUIInfo.selectedAllyUnit_AllyStats.hp, mapUIInfo.selectedAllyUnit_AllyStats.maxHP, 0);
+        }
+    }
+
+    public void SetFillAmounts(float currentHP, float maxHP, int healAmount)
+    {
+        Debug.Log(currentHP / maxHP);
+        CurrentHpHealthBar.fillAmount = (currentHP / maxHP);
+    }
+}
