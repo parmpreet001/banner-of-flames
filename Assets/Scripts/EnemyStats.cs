8c6de9848745159272850cbaff0e00e227ad6ab1
parmpreet001 <parmppreet1337@gmail.com>
2020-07-23 13:28:07 -0700

    Added EnemyBehavior enum

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyStats : Stats
 {
-    // Start is called before the first frame update
+    public EnemyBehavior enemyBehavior;
     void Start()
     {
         Init();
