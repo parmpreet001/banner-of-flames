311eb098aa9bda813d737d8b8869bee0f1f9768c
parmpreet001 <parmppreet1337@gmail.com>
2020-08-26 13:55:27 -0700

    Created UI_AdvancedInfoController. Upated UI/UI_AdvancedInfoDisplay

---
+++ Assets/Scripts/UI/UI_AdvancedInfoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_AdvancedInfoController : MonoBehaviour
+{
+    private UI_AdvancedInfoDisplay advancedInfoDisplay;
+    private MapUIInfo mapUIInfo;
+
+    private void Start()
+    {
+        advancedInfoDisplay = GetComponentInChildren<UI_AdvancedInfoDisplay>();
+        mapUIInfo = GetComponentInParent<MapUIInfo>();
+    }
+
+    private void Update()
+    {
+        if(mapUIInfo.selectedAllyUnit)
+        {
+            advancedInfoDisplay.gameObject.SetActive(true);
+            advancedInfoDisplay.UpdateStatsText(mapUIInfo.selectedAllyUnit_AllyStats.GetBattleStats());
+            advancedInfoDisplay.UpdateInventory(mapUIInfo.selectedAllyUnit_AllyStats.inventory);
+            advancedInfoDisplay.UpdateBasicInfo(mapUIInfo.selectedAllyUnit_AllyStats);
+        }
+        else
+        {
+            advancedInfoDisplay.gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdateInfo()
+    {
+
+    }
+}
--- Assets/Scripts/UI/UI_AdvancedInfoDisplay.cs
+++ Assets/Scripts/UI/UI_AdvancedInfoDisplay.cs
@@ -45,4 +45,45 @@ public class UI_AdvancedInfoDisplay : MonoBehaviour
             magicSlots[i] = new ItemSlot(magicDisplay.transform.GetChild(i).gameObject);
         }
     }
+
+    public void UpdateStatsText(int[] stats)
+    {
+        statsText1.text = stats[0] + "\n" + stats[1] + "\n" + stats[2] + "\n" + stats[3];
+        statsText2.text = stats[4] + "\n" + stats[5] + "\n" + stats[6] + "\n" + stats[7];
+    }
+
+    public void UpdateInventory(Item[] inventory)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (inventory[i])
+            {
+                itemSlots[i].name.text = inventory[i].name;
+                if(inventory[i].GetType() == typeof(Weapon))
+                {
+                    if((inventory[i] as Weapon).equipped)
+                        itemSlots[i].name.color = new Color32(34, 170, 160, 255);
+                    else
+                        itemSlots[i].name.color = Color.black;
+                }
+                else if(inventory[i].GetType() == typeof(HealingItem))
+                {
+                    itemSlots[i].uses.text = (inventory[i] as HealingItem).currentUses + "/" + (inventory[i] as HealingItem).maxUses;
+                }
+                else
+                {
+                    itemSlots[i].uses.text = "";
+                }
+            }
+            else
+            {
+                itemSlots[i].name.text = itemSlots[i].uses.text = "";
+            }
+        }
+    }
+
+    public void UpdateBasicInfo(AllyStats stats)
+    {
+        basicInfo.text = "Level: " + stats.level + "\n" + "Exerpience: " + stats.experience + "\n" + "Class: " + stats.classType.name;
+    }
 }
