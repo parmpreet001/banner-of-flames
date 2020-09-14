d155b5695c6f3ef6b6d80a8bf3222ec4f83aa7ca
Parmpreet <parmpreet1337@gmail.com>
2020-09-14 14:08:30 -0700

    Started making an image reader

--- Assets/Scripts/ImageReader.cs
+++ Assets/Scripts/ImageReader.cs
@@ -7,11 +7,17 @@ public class ImageReader : MonoBehaviour
     public Texture2D image;
 
     public GameObject grassObject;
+    public GameObject mountainObject;
+    public GameObject forestObject;
     private int mapLength;
     private int mapHeight;
 
-    private Color grass;
-    private Color enemyUnit;
+    private Color grassColor;
+    private Color enemyUnitColor;
+    private Color playerUnitColor;
+    private Color mountainColor;
+    private Color forestColor;
+    private Color waterColor;
 
     int grassCount;
 
@@ -22,29 +28,37 @@ public class ImageReader : MonoBehaviour
 
         Debug.Log(mapLength + "," + mapHeight);
 
-        grass = new Color32(144, 212, 97,255);
-        enemyUnit = new Color32(221, 41, 41,255);
+        grassColor = new Color32(144, 212, 97,255);
+        enemyUnitColor = new Color32(221, 41, 41,255);
+        playerUnitColor = new Color32(90, 40, 217, 255);
+        mountainColor = new Color32(174, 125, 46, 255);
+        forestColor = new Color32(58, 167, 32, 255);
 
         Color[] pixels = image.GetPixels();
         foreach(Color pixel in pixels)
         {
-            if(pixel.Equals(grass))
+            if(pixel.Equals(grassColor))
             {
                 grassCount++;
             }
         }
 
-        int pixelIndex = 0;
+
         for(int i = mapHeight; i > 0; i--)
         {
-            if(pixels[pixelIndex].Equals(grass))
+            for(int j = 0; j < mapLength; j++)
             {
-                Instantiate(grassObject, new Vector3(pixelIndex,mapHeight,0),Quaternion.identity);
+                if (pixels[j+((mapHeight-i)*mapHeight)].Equals(mountainColor))
+                {
+                    Debug.Log("here");
+                    Instantiate(mountainObject, new Vector3(j, i, 0), Quaternion.identity);
+                }
+                else if (pixels[j + ((mapHeight - i) * mapHeight)].Equals(grassColor))
+                {
+                    Instantiate(grassObject, new Vector3(j,i,0),Quaternion.identity);
+                }
             }
-            pixelIndex++;
         }
-
-        Debug.Log(grassCount);
     }
 
 }
